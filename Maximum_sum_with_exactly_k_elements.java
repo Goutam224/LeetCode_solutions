f8b5b00f509032d8e7f1508f4b2183a982e0046c
Goutam mukati <119522051+Goutam224@users.noreply.github.com>
2023-12-20 12:09:26 +0530

    Create Maximum_sum_with_exactly_k_elements.java

---
+++ Maximum_sum_with_exactly_k_elements.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximizeSum(int[] nums, int k) {
+       Arrays.sort(nums);
+       int sum=0;
+       int num=nums[nums.length-1];
+       for(int i=1;i<k;i++){
+            sum=sum+num+i;
+       }
+       return sum+num; 
+    }
+}
